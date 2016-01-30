8f8dc22cf66b40191a03787fec28b273bf464cf0
tyler <tyler@Kaydees-pc>
2016-01-30 14:54:06 -0700

    1/30/16-Kaydee's initial commit to repository

---
+++ src/byui/cit260/intoZnight/model/ConversationScene.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.intoZnight.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author kaydee
+ */
+public class ConversationScene implements Serializable{
+    //class instance variables
+    private String description;
+    private double travelDistance;
+
+    public ConversationScene() {
+    }
+    
+    
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getTravelDistance() {
+        return travelDistance;
+    }
+
+    public void setTravelDistance(double travelDistance) {
+        this.travelDistance = travelDistance;
+        
+        
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.description);
+        hash = 17 * hash + (int) (Double.doubleToLongBits(this.travelDistance) ^ (Double.doubleToLongBits(this.travelDistance) >>> 32));
+        return hash;
+        
+    }
+    
+      
+
+    @Override
+    public String toString() {
+        return "ConversationScene{" + "description=" + description + ", travelDistance=" + travelDistance + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ConversationScene other = (ConversationScene) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.travelDistance) != Double.doubleToLongBits(other.travelDistance)) {
+            return false;
+        }
+        return true;
+    }
+    
+    public void searchArea(){
+        
+    }
+    
+    public void leaveArea() {
+        
+    }
+    
+    public void talk() {
+    
+}
+}
