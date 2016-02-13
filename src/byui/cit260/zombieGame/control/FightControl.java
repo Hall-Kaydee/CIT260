adb599e0bc3125607efc77a9630a3320f2abbb07
Boyd <Boyd@Boyd-PC.attlocal.net>
2016-02-13 16:23:10 -0600

    This is my fight control class and test. There are actually two functions in fight control - calcEnemyHealth and calcCharacterHealth

--- src/byui/cit260/zombieGame/control/FightControl.java
+++ src/byui/cit260/zombieGame/control/FightControl.java
@@ -11,4 +11,61 @@ package byui.cit260.zombieGame.control;
  */
 public class FightControl {
     
+    public double calcEnemyHealth(double charDamage, double enemyHealth, int numberOfEnemies){
+ 
+         if (enemyHealth <= 0 || numberOfEnemies <= 0 || charDamage <=0){
+            
+            return -1;
+            }
+      
+        
+        
+        double healthPerEnemy = ((enemyHealth * numberOfEnemies) - charDamage) / numberOfEnemies;
+        
+        if (healthPerEnemy <= 0) {
+
+            System.out.println("Enemies are D.E.A.D."); 
+            
+		return healthPerEnemy;
+             }
+
+	else {
+            
+            return healthPerEnemy;
+            
+                }
+             }
+ 
+    
+    
+    public double calcCharacterHealth (double enemyDamage, double numberOfEnemies, double characterHealth, int charHitPoints ){
+    
+        if (numberOfEnemies <= 0 || characterHealth <= 0){
+            
+            return -1;
+            }
+        if (charHitPoints <= 0 || enemyDamage <= 0){
+            
+            return -1;
+            }       
+        
+        double charHpFactor;
+        double charHealth;
+        charHpFactor = characterHealth * charHitPoints / 100;
+        charHealth = characterHealth + charHpFactor - enemyDamage;
+
+ 
+        
+        if (charHealth <= 0) {
+
+                System.out.println("Character is D.E.A.D."); 
+		return charHealth;
+                }
+
+	else {
+            
+            return charHealth;
+            
+                }
+        }
 }
