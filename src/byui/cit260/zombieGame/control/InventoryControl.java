dd72588b2636defa89032e66fd0b293e0dc5b268
kaydeehall <kaydeehall@192.168.0.5>
2016-03-14 21:03:50 -0600

    individual assignment 3/14, alphabetical inventory sort

--- src/byui/cit260/zombieGame/control/InventoryControl.java
+++ src/byui/cit260/zombieGame/control/InventoryControl.java
@@ -12,9 +12,28 @@ import java.util.Scanner;
  * @author Evan
  */
 public class InventoryControl {
-    
-    
    
+    public void inventorySort( String  x [ ] )
+    {
+     
+            int i, j;
+            String temp;
+
+            for ( i = 0;  i < x.length - 1;  i++ )
+            {
+                for ( j = i + 1;  j < x.length;  j++ )
+                {  
+                         if ( x [ i ].compareToIgnoreCase( x [ j ] ) > 0 )
+                          {                                             // ascending sort
+                                      temp = x [ i ];
+                                      x [ i ] = x [ j ];    // swapping
+                                      x [ j ] = temp; 
+                                      
+                           } 
+                   } 
+             } 
+      } 
+    
    
     public void calcInventoryItem() {
 
